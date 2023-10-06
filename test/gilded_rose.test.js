e9d59fb17a09e838b7a6381f64647c45215d34d0
lsj2001 <lsj2001_lsj@hotmail.com>
2023-10-06 22:39:04 +1300

    Add unit tests for updateQuality method
    - Added tests to ensure correct behavior of normal items, Aged Brie, Sulfuras, Backstage passes, and Conjured items.
    - Ensure boundary conditions like quality never exceeding 50 or going below 0 are tested.

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,44 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+describe("Gilded Rose", function () {
+  // Test for normal items
+  it("should decrease quality and sellIn for normal items", function () {
+    const gildedRose = new Shop([new Item("Normal Item", 10, 20)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(19);
+  });
+
+  // Test for Aged Brie
+  it("should increase the quality of 'Aged Brie'", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(21);
+  });
+
+  // Test for Sulfuras
+  it("should not decrease quality or sellIn of 'Sulfuras'", function () {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 10, 80),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(10);
+    expect(items[0].quality).toBe(80);
+  });
+
+  // Test for Backstage passes
+  it("should increase the quality of 'Backstage passes' as its SellIn value approaches", function () {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(21);
+  });
+
+  // Test for Conjured items
+  it("should decrease quality of 'Conjured' items twice as fast as normal items", function () {
+    const gildedRose = new Shop([new Item("Conjured Mana Cake", 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(18);
   });
 });
